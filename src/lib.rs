ba3745c73fefa25198b98098af2e1acc1baf8cfa
nishan8583 <nishanmaharjan17@gmail.com>
2023-07-15 05:20:24 -0700

    yaml config file parsing actually working

--- src/lib.rs
+++ src/lib.rs
@@ -1,5 +1,10 @@
-use reqwest::{header};
+use reqwest::header;
 use serde_derive::{Deserialize,Serialize};
+use std::{io, fs};
+use std::io::Write;
+use serde_yaml;
+
+const  GPT_URL: &str = "https://api.openai.com/v1/chat/completions";
 
 #[derive(Serialize, Deserialize)]
 pub struct OpenAiResponse {
@@ -29,37 +34,79 @@ pub struct GptRequest<'a> {
     pub messages: Vec<Message>,
     pub temperature: f64,
 }
-const  GPT_URL: &str = "https://api.openai.com/v1/chat/completions";
 
-// get_chat_gpt_message takes a vector of Message, and API key
-pub fn get_chat_gpt_message(message: Vec<Message>, api_key: &str) -> String {
+#[derive(Serialize,Deserialize)]
+pub struct OpenAI {
+    pub api_key: String
+}
 
-    let body = GptRequest{
-        model:       "gpt-3.5-turbo",
-        messages: message,
-        temperature: 0.7,
-    };
+impl OpenAI{
+    pub fn get_chat_gpt_message(&self,message: String) -> String {
 
-    let req_body = serde_json::json!(body);
-    let body_str = req_body.to_string();
-
-    let request = reqwest::blocking::Client::new()
-    .post(GPT_URL)
-    .header(header::CONTENT_TYPE, "application/json")
-    .header(header::AUTHORIZATION, format!("Bearer {}",api_key))
-    .body(body_str)
-    .send()
-    .unwrap();
-
-
-    let response = request.text().unwrap();
-    let r: OpenAiResponse = serde_json::from_str(&response).unwrap();
-
-    return r.choices.get(0).unwrap().message.content.clone();
-
-
+        let message = vec![
+                    Message{
+                        role: "assistant".to_string(),
+                        content: message,
+                    }
+                ];
+        let body = GptRequest{
+            model:       "gpt-3.5-turbo",
+            messages: message,
+            temperature: 0.7,
+        };
     
+        let body_str = serde_json::json!(body).to_string();
+    
+        let response = reqwest::blocking::Client::new()
+        .post(GPT_URL)
+        .header(header::CONTENT_TYPE, "application/json")
+        .header(header::AUTHORIZATION, format!("Bearer {}",self.api_key))
+        .body(body_str)
+        .send()
+        .unwrap()
+        .text()
+        .unwrap();
+    
+    
+        let r: OpenAiResponse = serde_json::from_str(&response).unwrap();
+    
+        return r.choices.get(0).unwrap().message.content.clone();
+    
+    
+        
+    
+          
+    
+    }
+     
+    pub fn start_interactive(&self) {
+        let mut value = String::new();
+        loop{
+            print!("(you)>>> ");
+            io::stdout().flush().unwrap();
+            
+            // read input from line
+            io::stdin().read_line(&mut value).unwrap();
+            
+            // remove all the new line and carrot stuff
+            let value = value.replace("\n", "").replace("\r", "");
+            if value == "exit" || value =="quit" {
+                return;
+            }
+            
+            
+            print!("(gpt)>>> {}",self.get_chat_gpt_message(value));
+            println!("");
+            io::stdout().flush().unwrap();
+        }
+    }
+    
+}
 
-      
 
+
+pub fn parse_yaml() -> OpenAI {
+    let reader = std::fs::File::open("settings.yaml").unwrap();
+    let oai: OpenAI = serde_yaml::from_reader(reader).unwrap();
+    return oai;
 }
--- src/main.rs
+++ src/main.rs
@@ -1,11 +1,9 @@
-use std::io;
-use std::io::Write;
-
 use clap::{Command,Arg, ArgAction};
 use rustgpt::{*};
 
 fn main() {
 
+    let oai = parse_yaml();
     // getting command line arguements, I used builder pattern
     // derive pattern seems wierd to me
     let matches  = Command::new("program")
@@ -28,13 +26,7 @@ fn main() {
     match matches.get_one::<String>("message") {
         Some(v) => { 
             let message = v.clone();
-            let messages = vec![
-                Message{
-                    role: "assistant".to_string(),
-                    content: message,
-                }
-            ];
-            println!("{}",get_chat_gpt_message(messages, "<API_KEY>"));
+            println!("{}",oai.get_chat_gpt_message(message));
         },
         None => {},
     }
@@ -42,25 +34,7 @@ fn main() {
     // If they want to go interactive
     let value = matches.get_flag("interactive");
     if value {
-        loop{
-            print!("(you)>>> ");
-            io::stdout().flush().unwrap();
-            let mut value = String::new();
-            io::stdin().read_line(&mut value).unwrap();
-            let value = value.replace("\n", "").replace("\r", "");
-            if value == "exit" || value =="quit" {
-                return;
-            }
-            let messages = vec![
-                Message{
-                    role: "assistant".to_string(),
-                    content: value,
-                }
-            ];
-            
-            print!("(gpt)>>> {}",get_chat_gpt_message(messages, "<API_KEY"));
-            println!("");
-            io::stdout().flush().unwrap();
-        }
+        oai.start_interactive();
     } 
 }
+
